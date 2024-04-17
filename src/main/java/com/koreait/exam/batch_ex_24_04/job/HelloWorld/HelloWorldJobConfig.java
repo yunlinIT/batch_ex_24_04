478b8761757e877326656b7053bd7964a8598dde
yunlinIT <yunlinit@gmail.com>
2024-04-17 13:36:57 +0900

    배치 관련 DB, incrementer 설정, 매 실행시도마다 Job이 실행되도록

--- src/main/java/com/koreait/exam/batch_ex_24_04/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/job/HelloWorld/HelloWorldJobConfig.java
@@ -3,10 +3,9 @@ package com.koreait.exam.batch_ex_24_04.job.HelloWorld;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -14,13 +13,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloWorldConfig {
+public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID 를 실행할 때 파라미터로 부여
                 .start(helloWorldStep1())
                 .build();
     }
