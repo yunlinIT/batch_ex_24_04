5e229a3813eb3cda95155379b13ed6f1b1492fdb
yunlinIT <yunlinit@gmail.com>
2024-04-18 16:56:25 +0900

    동일 회원이 같은 상품을 2번 이상 장바구니에 넣으면 기존의 수량을 수정

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/cart/repository/CartItemRepository.java
@@ -1,8 +1,10 @@
-
 package com.koreait.exam.batch_ex_24_04.app.cart.repository;
 
 import com.koreait.exam.batch_ex_24_04.app.cart.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByMemberIdAndProductOptionId(long memberId, long cartOptionId);
 }
