a94d2908d311b2fc27e7b3273dc2e66b36151e2b
yunlinIT <yunlinit@gmail.com>
2024-04-19 16:25:25 +0900

    통계 및 정산을 위한 컬럼들 추가

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/product/entity/ProductOption.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/product/entity/ProductOption.java
@@ -23,9 +23,14 @@ public class ProductOption extends BaseEntity {
     private String size;
     private String displayColor;
     private String displaySize;
-    private int price;
-    private int salePrice;
-    private int wholesalePrice;
+    private int price; // 권장 판매가 53000
+    private int salePrice; // 실제 판매가 39800
+    private int wholesalePrice; // 도매가 < 53000
+    private int payPrice; // 결제금액
+    private int refundPrice; // 환불금액
+    private int pgFee; // 결제대행사 수수료
+    private int refundQuantity; // 환불 한 갯수
+    private boolean isPaid;  // 결제 여부
 
     @ManyToOne(fetch = LAZY)
     @ToString.Exclude
