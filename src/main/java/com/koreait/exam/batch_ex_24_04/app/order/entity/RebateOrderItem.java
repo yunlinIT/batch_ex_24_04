e8ba523eef660438300f6b3668ed31a5e125d078
yunlinIT <yunlinit@gmail.com>
2024-04-23 16:52:14 +0900

    이전 커밋에서 오류 수정

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/order/entity/RebateOrderItem.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/order/entity/RebateOrderItem.java
@@ -1,25 +1,35 @@
+package com.koreait.exam.batch_ex_24_04.app.order.entity;
+
 import com.koreait.exam.batch_ex_24_04.app.base.entity.BaseEntity;
-import com.koreait.exam.batch_ex_24_04.app.order.entity.Order;
-import com.koreait.exam.batch_ex_24_04.app.order.entity.OrderItem;
 import com.koreait.exam.batch_ex_24_04.app.product.entity.ProductOption;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
 public class RebateOrderItem extends BaseEntity {
 
     @OneToOne(fetch = FetchType.LAZY)
     @ToString.Exclude
-    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private OrderItem orderItem;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @ToString.Exclude
-    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Order order;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private ProductOption productOption;
 
     private int quantity;
@@ -33,3 +43,46 @@ public class RebateOrderItem extends BaseEntity {
     private int refundPrice; // 환불금액
     private int refundQuantity; // 환불 한 갯수
     private boolean isPaid;  // 결제 여부
+
+
+    public RebateOrderItem(OrderItem orderItem) {
+        this.orderItem = orderItem;
+        order = orderItem.getOrder();
+        productOption = orderItem.getProductOption();
+        quantity = orderItem.getQuantity();
+        price = orderItem.getPrice();
+        salePrice = orderItem.getSalePrice();
+        wholesalePrice = orderItem.getWholesalePrice();
+        pgFee = orderItem.getPgFee();
+        payPrice = orderItem.getPayPrice();
+        refundPrice = orderItem.getRefundPrice();
+        refundQuantity = orderItem.getRefundQuantity();
+        isPaid = orderItem.isPaid();
+    }
+
+    public RebateOrderItem(ProductOption productOption, int quantity) {
+        this.productOption = productOption;
+        this.quantity = quantity;
+        this.price = productOption.getPrice();
+        this.salePrice = productOption.getSalePrice();
+        this.wholesalePrice = productOption.getWholesalePrice();
+    }
+
+    public int calculatePayPrice() {
+        return salePrice * quantity;
+    }
+
+    public void setPaymentDone() {
+        this.pgFee = 0;
+        this.payPrice = calculatePayPrice();
+        this.isPaid = true;
+    }
+
+    public void setRefundDone() {
+        if(refundQuantity == quantity) return;
+
+        this.refundQuantity = quantity;
+        this.refundPrice = payPrice;
+
+    }
+}
