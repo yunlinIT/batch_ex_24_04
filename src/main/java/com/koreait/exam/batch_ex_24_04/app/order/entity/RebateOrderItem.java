9ab3b689c7cdeb69c65259c1815d303dbab74925
yunlinIT <yunlinit@gmail.com>
2024-04-25 13:47:40 +0900

    주문 품목의 생성날짜를 정산 테이블의 컬럼으로 적용

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/order/entity/RebateOrderItem.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/order/entity/RebateOrderItem.java
@@ -9,6 +9,7 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -44,15 +45,17 @@ public class RebateOrderItem extends BaseEntity {
     private int refundQuantity; // 환불 한 갯수
     private boolean isPaid;  // 결제 여부
 
-    //상품
+    // 상품
     private String productName;
 
-    //상품옵션
+    // 상품 옵션
     private String productOptionColor;
     private String productOptionSize;
     private String productOptionDisplayColor;
     private String productOptionDisplaySize;
 
+    // 주문 품목
+    private LocalDateTime orderItemCreateDate;
 
     public RebateOrderItem(OrderItem orderItem) {
         this.orderItem = orderItem;
@@ -68,17 +71,58 @@ public class RebateOrderItem extends BaseEntity {
         refundQuantity = orderItem.getRefundQuantity();
         isPaid = orderItem.isPaid();
 
-        //상품
+        // 상품 추가데이터
         productName= orderItem.getProductOption().getProduct().getName();
 
-        //상품옵션
+        // 상품옵션 추가데이터
         productOptionColor = orderItem.getProductOption().getColor();
         productOptionSize = orderItem.getProductOption().getSize();
         productOptionDisplayColor = orderItem.getProductOption().getDisplayColor();
         productOptionDisplaySize = orderItem.getProductOption().getDisplaySize();
 
+        // 주문 품목 추가데이터
+        orderItemCreateDate = orderItem.getCreateDate();
+
     }
+//    # 기존
+//    SELECT ROI.order_item_id AS `주문품목 번호`,
+//    DATE(OI.create_date) AS `주문날짜`,
+//    CONCAT(FORMAT(ROI.pay_price - ROI.refund_price, 0), '원') AS `결제금액`,
+//    P.name AS `상품명`,
+//    PO.color AS `색상`,
+//    PO.size AS `사이즈`,
+//    ROI.quantity - ROI.refund_quantity AS `개수`
+//    FROM rebate_order_item AS ROI
+//    INNER JOIN order_item AS OI
+//    ON ROI.order_item_id = OI.id
+//    INNER JOIN product_option AS PO
+//    ON ROI.product_option_id = PO.id
+//    INNER JOIN product AS P
+//    ON PO.product_id = P.id
+//    ORDER BY ROI.order_item_id ASC;
+//
+//    # 개선 - 1
+//    SELECT ROI.order_item_id AS `주문품목 번호`,
+//    DATE(OI.create_date) AS `주문날짜`,
+//    CONCAT(FORMAT(ROI.pay_price - ROI.refund_price, 0), '원') AS `결제금액`,
+//    ROI.product_name AS `상품명`,
+//    ROI.product_option_color AS `색상`,
+//    ROI.product_option_size AS `사이즈`,
+//    ROI.quantity - ROI.refund_quantity AS `개수`
+//    FROM rebate_order_item AS ROI
+//    INNER JOIN order_item AS OI
+//    ON ROI.order_item_id = OI.id
+//    ORDER BY ROI.order_item_id ASC;
 
-
+//    # 개선 - 2
+//    SELECT ROI.order_item_id AS `주문 품목 번호`,
+//    DATE(ROI.order_item_create_date) AS `주문 날짜`,
+//    CONCAT(FORMAT(ROI.pay_price - ROI.refund_price,0),'원') AS `결제금액`,
+//    ROI.product_name AS `상품명`,
+//    ROI.product_option_color AS `색상`,
+//    ROI.product_option_size AS `사이즈`,
+//    ROI.quantity - ROI.refund_quantity AS `수량`
+//    FROM rebate_order_item AS ROI
+//    ORDER BY ROI.order_item_id ASC;
 
 }
